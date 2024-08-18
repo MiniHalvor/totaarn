85e96a6f65acf8c9e31673088ba23577a9387987
Halvortm <halvor.munkeby@gmail.com>
2024-08-18 17:36:01 +0200

    0.1.1 Header completed :)

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,31 @@
-Janus 
+<script>
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+	<section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+		<div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+			<h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+				Velkommen til <span class="poppins text-violet-400"><br />Janus:</span> To Tårn
+			</h2>
+
+			<p class="text-base sm:text-lg md:text-xl">
+				Vi er en sjakklubb for <span class="text-violet-400">alle</span> indøkere, enten du kan spillet
+				eller ei. Vi møtes en gang i uken for å spille sjakk og hygge oss. I tillegg arrangerer vi en
+				sjakk-turnering i året, for å kåre indøks beste sjakkspiller.
+			</p>
+
+			<a
+				class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+			href="#"
+            target="_blank">
+				<div
+					class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+				/>
+				<h4 class="relative z-9">Kontakt &rarr;</h4></a
+			>
+		</div>
+        <div class="relative shadow-2xl grid place-items-center">
+            <img src={"favicon.png"} alt="sexy bilde av arthur" class="object-cover z-[2] max-h-[70vh]"/>
+        </div>
+	</section>
+</main>
