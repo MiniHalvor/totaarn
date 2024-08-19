dd9534a08f2500a6411028bb7e19c1a702f11da4
Halvortm <halvor.munkeby@gmail.com>
2024-08-19 14:43:24 +0200

    vetikke

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -25,7 +25,7 @@
 			>
 		</div>
         <div class="relative shadow-2xl grid place-items-center">
-            <img src={"JTT-logo.png"} alt="sexy bilde av arthur" class="object-cover z-[2] max-h-[70vh]"/>
+            <img src={"JTT-logo.png"} alt="logo til to tårn" class="object-cover z-[2] max-h-[70vh]"/>
         </div>
 	</section>
 </main>
