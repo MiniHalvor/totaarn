601f0920c83971defd7568a75816217f1d77fbec
Halvortm <halvor.munkeby@gmail.com>
2024-09-08 15:48:38 +0200

    en passant er live, og du kan faktisk sette kongen i sjakk uten at han kan ignorere det

--- src/routes/janusCarlsen/+page.svelte
+++ src/routes/janusCarlsen/+page.svelte
@@ -30,12 +30,33 @@
 		[1, 1, 1, 1, 1, 1, 1, 1],
 		[0, 1, 1, 1, 1, 1, 1, 0]
 	];
+	let enPassantWhite = [
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0]
+	];
+	let enPassantBlack = [
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0]
+	];
 	const blackPieces = ['p', 'q', 'k', 'b', 'n', 'r'];
 	const whitePieces = ['P', 'Q', 'K', 'B', 'N', 'R'];
 	let colorToMove = whitePieces;
 	let board = initialBoard;
-	let debug = 'Sjakken er ganske broken nå. Du kan ikke sette seg selv i sjakk, men du trenger ikke bry deg hvis du blir satt i sjakk heller. En passant og rokkade er ikke adda enda. De røde bokstavene indikerer hvilke felt som er angrepet av hvit -> W og sort -> B. <br><br> Det jeg mangler er altså:'+
-	'<br>-->En passant <br>-->Rokkade <br>-->Arthur 1.1 <br>-->sjakk-matt ';
+	let debug =
+		'Sjakken er ganske broken nå. Du kan ikke sette seg selv i sjakk, men du trenger ikke bry deg hvis du blir satt i sjakk heller. En passant bør funke, men har ikke testa grundig nok enda. Rokkade er ikke adda enda. De røde bokstavene indikerer hvilke felt som er angrepet av hvit -> W og sort -> B. <br><br> Det jeg mangler er altså:' +
+		'<br>--><s>En passant</s> <br>-->Rokkade <br>-->Arthur 1.1 <br>-->sjakk-matt ';
 	// Mapping pieces to image file paths
 	const pieceSymbols = {
 		r: 'black-rook.png',
@@ -54,6 +75,7 @@
 
 	let selectedPiece = null;
 	let selectedFrom = { row: null, col: null };
+	let pause = false;
 	function changeTurns() {
 		if (colorToMove[0] == whitePieces[0]) {
 			colorToMove = blackPieces;
@@ -64,29 +86,28 @@
 	function sleep(ms) {
 		return new Promise((resolve) => setTimeout(resolve, ms));
 	}
-	function containsKings() {
-		const flatBoard = board.flat();
-		const whiteKingExists = flatBoard.includes('K');
-		const blackKingExists = flatBoard.includes('k');
-		return whiteKingExists && blackKingExists;
-	}
-	function isKingInCheck(tempBoard) {
+
+	function isKingInCheck(tempBoard, strikeTable, kingColor) {
 		//send in tempboard to simulate moves that may end up in discovered check
-		//the way it is made right now doesnt make sure that discovered checks are forbidden
+
 		for (let i = 0; i < 8; i++) {
 			for (let j = 0; j < 8; j++) {
-				if (tempBoard[i][j] == 'k' && attackedByWhite[i][j] == 1) {
+				if (tempBoard[i][j] == 'k' && tempBoard[i][j] == kingColor && strikeTable[i][j] == 1) {
 					return true;
 				}
-				if (tempBoard[i][j] == 'K' && attackedByBlack[i][j] == 1) {
+				if (tempBoard[i][j] == 'K' && tempBoard[i][j] == kingColor && strikeTable[i][j] == 1) {
 					return true;
 				}
 			}
 		}
 	}
+	function stopRandomGame() {
+		pause = true;
+	}
 
 	async function startRandomGame() {
-		while (containsKings()) {
+		pause = false;
+		while (!pause) {
 			// Ensure game continues while both kings are on the board
 			let pieceFound = false;
 			let rand1 = 0,
@@ -110,89 +131,134 @@
 			handleSquareClick(rand3, rand4); // Try to move to the new location
 
 			// page freezes without the sleep. sorry
-			await sleep(1);
+			await sleep(0);
 		}
 	}
 
-	function updateStrikeTable() {
+	function clearStrikeTable() {
+		attackedByBlack = [
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0]
+		];
+		attackedByWhite = [
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0]
+		];
+	}
+	function clearEnPassantTable() {
+		enPassantBlack = [
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0]
+		];
+		enPassantWhite = [
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0]
+		];
+	}
+
+	function updateStrikeTable(tempBoard) {
+		clearStrikeTable();
 		for (let i = 0; i < 8; i++) {
 			for (let j = 0; j < 8; j++) {
-				if (!board[i][j]) continue; // Skip empty squares
+				if (!tempBoard[i][j]) continue; // Skip empty squares
 
 				//Iterate through alle the pieces on the board
 				function updateStrikeTableForRooks() {
 					// as a fucntion so that when i get to the queen i can call upon this function and the bishop function
-					if (board[i][j] == board[i][j].toLowerCase()) {
+					if (tempBoard[i][j] == tempBoard[i][j].toLowerCase()) {
 						for (let k = j + 1; k < 8; k++) {
-							if (board[i][k] != null) {
-								attackedByBlack[i][k] = 1;
+							attackedByBlack[i][k] = 1;
+							if (tempBoard[i][k] != null) {
+								if (tempBoard[i][k] === 'K') {
+									// If it's the white king, mark this square but continue
+									continue;
+								}
 								break;
-							} else {
-								attackedByBlack[i][k] = 1;
 							}
 						}
 						for (let k = j - 1; k > 0; k--) {
-							if (board[i][k] != null) {
-								attackedByBlack[i][k] = 1;
+							attackedByBlack[i][k] = 1;
+							if (tempBoard[i][k] != null) {
+								if (tempBoard[i][k] === 'K') {
+									continue;
+								}
+
 								break;
-							} else {
-								attackedByBlack[i][k] = 1;
 							}
 						}
 						for (let k = i + 1; k < 8; k++) {
-							if (board[k][j] != null) {
-								attackedByBlack[k][j] = 1;
+							attackedByBlack[k][j] = 1;
+							if (tempBoard[k][j] != null) {
+								if (tempBoard[k][j] === 'K') {
+									continue;
+								}
 								break;
-							} else {
-								attackedByBlack[k][j] = 1;
 							}
 						}
 						for (let k = i - 1; k > 0; k--) {
-							if (board[k][j] != null) {
-								attackedByBlack[k][j] = 1;
+							attackedByBlack[k][j] = 1;
+							if (tempBoard[k][j] != null) {
+								if (tempBoard[k][j] === 'K') {
+									continue;
+								}
+
 								break;
-							} else {
-								attackedByBlack[k][j] = 1;
 							}
 						}
-					} else if (board[i][j] == board[i][j].toUpperCase()) {
+					} else if (tempBoard[i][j] == tempBoard[i][j].toUpperCase()) {
 						for (let k = j + 1; k < 8; k++) {
-							if (board[i][k] != null) {
-								attackedByWhite[i][k] = 1;
+							attackedByWhite[i][k] = 1;
+							if (tempBoard[i][k] != null && tempBoard[i][k] != 'k') {
 								break;
-							} else {
-								attackedByWhite[i][k] = 1;
 							}
 						}
 						for (let k = j - 1; k > 0; k--) {
-							if (board[i][k] != null) {
-								attackedByWhite[i][k] = 1;
+							attackedByWhite[i][k] = 1;
+							if (tempBoard[i][k] != null && tempBoard[i][k] != 'k') {
 								break;
-							} else {
-								attackedByWhite[i][k] = 1;
 							}
 						}
 						for (let k = i + 1; k < 8; k++) {
-							if (board[k][j] != null) {
-								attackedByWhite[k][j] = 1;
+							attackedByWhite[k][j] = 1;
+							if (tempBoard[k][j] != null && tempBoard[k][j] != 'k') {
 								break;
-							} else {
-								attackedByWhite[k][j] = 1;
 							}
 						}
 						for (let k = i - 1; k > 0; k--) {
-							if (board[k][j] != null) {
-								attackedByWhite[k][j] = 1;
+							attackedByWhite[k][j] = 1;
+							if (tempBoard[k][j] != null && tempBoard[k][j] != 'k') {
 								break;
-							} else {
-								attackedByWhite[k][j] = 1;
 							}
 						}
 					}
 				}
 				function updateStrikeTableForKnights() {
 					//completed for knights
-					if (board[i][j] == 'n') {
+					if (tempBoard[i][j] == 'n') {
 						const potPairs = [
 							[i - 2, j - 1],
 							[i - 1, j - 2],
@@ -214,7 +280,7 @@
 								attackedByBlack[potPairs[k][0]][potPairs[k][1]] = 1;
 							}
 						}
-					} else if (board[i][j] == 'N') {
+					} else if (tempBoard[i][j] == 'N') {
 						const potPairs = [
 							[i - 2, j - 1],
 							[i - 1, j - 2],
@@ -240,70 +306,54 @@
 				}
 				function updateStrikeTableForBishops() {
 					//completed
-					if (board[i][j] == board[i][j].toLowerCase()) {
+					if (tempBoard[i][j] == tempBoard[i][j].toLowerCase()) {
 						for (let k = i + 1, p = j + 1; k < 8 && p < 8; k++, p++) {
-							if (board[k][p] != null) {
-								attackedByBlack[k][p] = 1;
+							attackedByBlack[k][p] = 1;
+							if (tempBoard[k][p] != null && tempBoard[k][p] != 'K') {
 								break;
-							} else {
-								attackedByBlack[k][p] = 1;
 							}
 						}
 						for (let k = i + 1, p = j - 1; k < 8 && p > -1; k++, p--) {
-							if (board[k][p] != null) {
-								attackedByBlack[k][p] = 1;
+							attackedByBlack[k][p] = 1;
+							if (tempBoard[k][p] != null && tempBoard[k][p] != 'K') {
 								break;
-							} else {
-								attackedByBlack[k][p] = 1;
 							}
 						}
 						for (let k = i - 1, p = j - 1; k > -1 && p > -1; k--, p--) {
-							if (board[k][p] != null) {
-								attackedByBlack[k][p] = 1;
+							attackedByBlack[k][p] = 1;
+							if (tempBoard[k][p] != null && tempBoard[k][p] != 'K') {
 								break;
-							} else {
-								attackedByBlack[k][p] = 1;
 							}
 						}
 						for (let k = i - 1, p = j + 1; k > -1 && p < 8; k--, p++) {
-							if (board[k][p] != null) {
-								attackedByBlack[k][p] = 1;
+							attackedByBlack[k][p] = 1;
+							if (tempBoard[k][p] != null && tempBoard[k][p] != 'K') {
 								break;
-							} else {
-								attackedByBlack[k][p] = 1;
 							}
 						}
 					} else {
 						for (let k = i + 1, p = j + 1; k < 8 && p < 8; k++, p++) {
-							if (board[k][p] != null) {
-								attackedByWhite[k][p] = 1;
+							attackedByWhite[k][p] = 1;
+							if (tempBoard[k][p] != null && tempBoard[k][p] != 'k') {
 								break;
-							} else {
-								attackedByWhite[k][p] = 1;
 							}
 						}
 						for (let k = i + 1, p = j - 1; k < 8 && p > -1; k++, p--) {
-							if (board[k][p] != null) {
-								attackedByWhite[k][p] = 1;
+							attackedByWhite[k][p] = 1;
+							if (tempBoard[k][p] != null && tempBoard[k][p] != 'k') {
 								break;
-							} else {
-								attackedByWhite[k][p] = 1;
 							}
 						}
 						for (let k = i - 1, p = j - 1; k > -1 && p > -1; k--, p--) {
-							if (board[k][p] != null) {
-								attackedByWhite[k][p] = 1;
+							attackedByWhite[k][p] = 1;
+							if (tempBoard[k][p] != null && tempBoard[k][p] != 'k') {
 								break;
-							} else {
-								attackedByWhite[k][p] = 1;
 							}
 						}
 						for (let k = i - 1, p = j + 1; k > -1 && p < 8; k--, p++) {
-							if (board[k][p] != null) {
-								attackedByWhite[k][p] = 1;
+							attackedByWhite[k][p] = 1;
+							if (tempBoard[k][p] != null && tempBoard[k][p] != 'k') {
 								break;
-							} else {
-								attackedByWhite[k][p] = 1;
 							}
 						}
 					}
@@ -316,7 +366,7 @@
 				function updateStrikeTableForKings() {
 					//completed
 					//completed
-					if (board[i][j] == board[i][j].toLowerCase()) {
+					if (tempBoard[i][j] == tempBoard[i][j].toLowerCase()) {
 						const potPairs = [
 							[i + 1, j + 1],
 							[i + 1, j],
@@ -365,7 +415,7 @@
 				function updateStrikeTableForPawns() {
 					//completed
 
-					if (board[i][j] == board[i][j].toLowerCase()) {
+					if (tempBoard[i][j] == tempBoard[i][j].toLowerCase()) {
 						const potPairs = [
 							[i + 1, j + 1],
 							[i + 1, j - 1]
@@ -399,17 +449,17 @@
 						}
 					}
 				}
-				if (board[i][j].toLowerCase() == 'r') {
+				if (tempBoard[i][j].toLowerCase() == 'r') {
 					updateStrikeTableForRooks();
-				} else if (board[i][j].toLowerCase() == 'n') {
+				} else if (tempBoard[i][j].toLowerCase() == 'n') {
 					updateStrikeTableForKnights();
-				} else if (board[i][j].toLowerCase() == 'b') {
+				} else if (tempBoard[i][j].toLowerCase() == 'b') {
 					updateStrikeTableForBishops();
-				} else if (board[i][j].toLowerCase() == 'q') {
+				} else if (tempBoard[i][j].toLowerCase() == 'q') {
 					updateStrikeTableForQueens();
-				} else if (board[i][j].toLowerCase() == 'k') {
+				} else if (tempBoard[i][j].toLowerCase() == 'k') {
 					updateStrikeTableForKings();
-				} else if (board[i][j].toLowerCase() == 'p') {
+				} else if (tempBoard[i][j].toLowerCase() == 'p') {
 					updateStrikeTableForPawns();
 				}
 			}
@@ -419,28 +469,95 @@
 	// Function to handle square clicks
 	const handleSquareClick = (rowIndex, colIndex) => {
 		function movePiece() {
-			//Promote to queen if pawn goes all the way
-			let tempBoard = board;
+			let tempBoard = structuredClone(board);
+
+			let originalAttackedByBlack = structuredClone(attackedByBlack);
+			let originalAttackedByWhite = structuredClone(attackedByWhite);
+			if (
+				selectedPiece == 'P' &&
+				tempBoard[rowIndex][colIndex] == null &&
+				selectedFrom.col != colIndex
+			) {
+				//hvis en passant
+				tempBoard[rowIndex + 1][colIndex] = null;
+			}
+			if (
+				selectedPiece == 'p' &&
+				tempBoard[rowIndex][colIndex] == null &&
+				selectedFrom.col != colIndex
+			) {
+				//hvis en passant
+				tempBoard[rowIndex - 1][colIndex] = null;
+			}
+
 			tempBoard[rowIndex][colIndex] = selectedPiece;
 			tempBoard[selectedFrom.row][selectedFrom.col] = null;
-			if (!isKingInCheck(board) && !isKingInCheck(tempBoard)) {
-				if (selectedPiece === 'p' && rowIndex % 7 === 0) {
-					board[rowIndex][colIndex] = 'q';
-					board[selectedFrom.row][selectedFrom.col] = null;
-					changeTurns();
-				} else if (selectedPiece === 'P' && rowIndex % 7 === 0) {
-					board[rowIndex][colIndex] = 'Q';
-					board[selectedFrom.row][selectedFrom.col] = null;
-					changeTurns();
-				} else {
+			updateStrikeTable(tempBoard);
+
+			if (whitePieces.includes(selectedPiece)) {
+				//hvis valgt brikke er hvit
+
+				if (!isKingInCheck(tempBoard, attackedByBlack, 'K')) {
+					//hvis hvit konge ikke er under angrep etter flyttet
+
+					if (selectedPiece == 'P' && Math.abs(selectedFrom.row - rowIndex) == 2) {
+						clearEnPassantTable();
+						enPassantBlack[rowIndex + 1][colIndex] = 1;
+
+						console.log(enPassantBlack);
+					}
+					if (
+						selectedPiece == 'P' &&
+						board[rowIndex][colIndex] == null &&
+						selectedFrom.col != colIndex
+					) {
+						//hvis en passant
+						board[rowIndex + 1][colIndex] = null;
+					}
 					board[rowIndex][colIndex] = selectedPiece;
 					board[selectedFrom.row][selectedFrom.col] = null;
+
+					promoteToQueen();
+
 					changeTurns();
 				}
-				updateStrikeTable();
+			} else if (blackPieces.includes(selectedPiece)) {
+				//hvis valgt brikke er svart og ikke under angrep ettter flyttet
+				if (!isKingInCheck(tempBoard, attackedByWhite, 'k')) {
+					//hvis hvit konge ikke er under angrep etter flyttet
+
+					if (selectedPiece == 'p' && Math.abs(selectedFrom.row - rowIndex) == 2) {
+						clearEnPassantTable();
+						enPassantWhite[rowIndex - 1][colIndex] = 1;
+
+						console.log(enPassantWhite);
+					}
+					if (
+						selectedPiece == 'p' &&
+						board[rowIndex][colIndex] == null &&
+						selectedFrom.col != colIndex
+					) {
+						//hvis en passant
+						board[rowIndex - 1][colIndex] = null;
+					}
+
+					board[rowIndex][colIndex] = selectedPiece;
+					board[selectedFrom.row][selectedFrom.col] = null;
+					promoteToQueen();
+
+					changeTurns();
+				}
+			}
+			updateStrikeTable(board);
+		}
+		function promoteToQueen() {
+			if (selectedPiece === 'p' && rowIndex % 7 === 0) {
+				board[rowIndex][colIndex] = 'q';
+			} else if (selectedPiece === 'P' && rowIndex % 7 === 0) {
+				board[rowIndex][colIndex] = 'Q';
 			}
 		}
-		// If a piece is already selected, move it to the new square
+
 		if (colorToMove.includes(selectedPiece)) {
 			// Check if the piece is being moved to a different square
 			if (selectedFrom.row !== rowIndex || selectedFrom.col !== colIndex) {
@@ -448,7 +565,6 @@
 				if (targetPiece && targetPiece.toLowerCase() === 'k') {
 					console.log('cannot capture king');
 				} else {
-					// checks if the piece it moves to
 					if (
 						!(whitePieces.includes(selectedPiece)
 							? whitePieces.includes(board[rowIndex][colIndex])
@@ -467,18 +583,22 @@
 								selectedPiece === 'p'
 									? whitePieces.includes(
 											board[selectedFrom.row + 1 * pawnDirection][selectedFrom.col + 1]
-										)
+										) ||
+										enPassantBlack[selectedFrom.row + 1 * pawnDirection][selectedFrom.col + 1] == 1
 									: blackPieces.includes(
 											board[selectedFrom.row + 1 * pawnDirection][selectedFrom.col + 1]
-										);
+										) ||
+										enPassantWhite[selectedFrom.row + 1 * pawnDirection][selectedFrom.col + 1] == 1;
 							const pawnCanStrikeLeft =
 								selectedPiece === 'p'
 									? whitePieces.includes(
 											board[selectedFrom.row + 1 * pawnDirection][selectedFrom.col - 1]
-										)
+										) ||
+										enPassantBlack[selectedFrom.row + 1 * pawnDirection][selectedFrom.col - 1] == 1
 									: blackPieces.includes(
 											board[selectedFrom.row + 1 * pawnDirection][selectedFrom.col - 1]
-										);
+										) ||
+										enPassantWhite[selectedFrom.row + 1 * pawnDirection][selectedFrom.col - 1] == 1;
 							if (
 								((pawnDistance === 2 && isFirstMove) || pawnDistance === 1) &&
 								(colIndex === selectedFrom.col ||
@@ -1062,7 +1182,8 @@
 				<div
 					class={`relative flex justify-center items-center w-full h-full cursor-pointer ${rowIndex % 2 === colIndex % 2 ? 'bg-[#f0d9b5]' : 'bg-[#b58863]'} ${selectedFrom.row === rowIndex && selectedFrom.col === colIndex ? 'border-4 border-violet-400' : ''}`}
 					on:click={() => handleSquareClick(rowIndex, colIndex)}
-				> <!--diven har class relative for at barna under skal kunne ligge oppå-->
+				>
+					<!--diven har class relative for at barna under skal kunne ligge oppå-->
 					{#if piece}
 						<img src={`chess-pieces/${pieceSymbols[piece]}`} alt="chess piece" class="piece" />
 					{/if}
@@ -1086,19 +1207,22 @@
 		{/each}
 	</div>
 	<div class=" sm:w-full md:w-1/2 aspect-square border-2 border-gray-800">
-		<!-- Additional content or controls can go here -->
 		<button
 			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
 			on:click={startRandomGame}>Se Arthur spille!</button
+		>
+		<button
+			class="bg-blue-500 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
+			on:click={stopRandomGame}>Pause</button
 		>
 		<p>{@html debug}</p>
 	</div>
 </main>
 
 <style>
-	/* Ensure the chess pieces fit well within the squares */
-	.piece {
-		width: 100%; /* Scale pieces to fit within squares */
-		height: auto; /* Maintain aspect ratio */
-	}
+	/* legacy code, but may have to bring back to life if things stop working
+	/* .piece {
+		width: 100%; 
+		height: auto; 
+	} */
 </style>
